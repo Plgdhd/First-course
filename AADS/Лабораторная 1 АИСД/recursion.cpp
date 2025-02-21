4fcb9738db7f9f5d0e0bbc6f7fe0476d00fd92a3
Plgdhd <aivanuk161@gmail.com>
2025-02-21 16:42:46 +0300

    backup

--- AADS/Лабораторная 1 АИСД/recursion.cpp
+++
@@ -1,6 +0,0 @@
-#include "Headers.h";
-unsigned long long int recursionProcessing(long long number) {
-	if (number == 0) return number;
-	if (number == 1) return number;
-	return recursionProcessing(number - 1) + recursionProcessing(number - 2);
-}
