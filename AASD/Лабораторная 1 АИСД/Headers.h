5ea1c6c30e94448daf33fdcef8d361b63e944ccf
Plgdhd <aivanuk161@gmail.com>
2025-02-21 16:44:52 +0300

    AASD

---
+++ AASD/Лабораторная 1 АИСД/Headers.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <Windows.h>
+
+using namespace std;
+using namespace chrono;
+
+unsigned long long int cycleProcessing(long long number);
+unsigned long long int recursionProcessing(long long number);
---
+++ AASD/Лабораторная 1 АИСД/main.cpp
@@ -0,0 +1,42 @@
+﻿#include "Headers.h"
+void main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите число: ";
+	long long number = 0;
+	try {
+		
+		cin >> number;
+
+		if (cin.fail() || number < 0) {
+			throw invalid_argument("Неверный ввод!");
+		}
+
+		steady_clock::time_point begin = steady_clock::now();
+
+		cout << "Обработка циклом: " << cycleProcessing(number);
+
+		steady_clock::time_point end = steady_clock::now();
+		cout << ". Время обработки: " << duration_cast<minutes>(end - begin).count() << " мин. " <<
+			duration_cast<seconds>(end - begin).count() << " сек. " << duration_cast<microseconds>(end - begin).count() <<
+			" мс. ";
+
+		Sleep(200);
+
+		begin = steady_clock::now();
+
+		cout << endl << "Обработка рекурсией: " << cycleProcessing(number);
+
+		end = steady_clock::now();
+		cout << ". Время обработки: " << duration_cast<minutes>(end - begin).count() << " мин. " <<
+			duration_cast<seconds>(end - begin).count() << " сек. " << duration_cast<microseconds>(end - begin).count() <<
+			" мс. ";
+		Sleep(1000);
+		cout << endl << "Удачи!";
+	}
+	catch (const invalid_argument& e) {
+		cout << "Ошибка: " << e.what();
+		return;
+	};
+}
